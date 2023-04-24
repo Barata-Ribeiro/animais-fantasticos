9a21c958af956ddecef2950534afc02e3ad3237a
João M J Barata Ribeiro <j.mendes1920@gmail.com>
2023-04-24 06:49:30 -0300

    SmoothScroll transformado em Classe

--- js/modules/smooth-scroll.js
+++ js/modules/smooth-scroll.js
@@ -1,18 +1,30 @@
-export default function initSmoothScroll() {
-    const internalLinks = document.querySelectorAll('[data-menu="suave"] a[href^="#"]');
+export default class SmoothScroll {
+    constructor(links, options) {
+        this.internalLinks = document.querySelectorAll(links);
+        if (options === undefined) {
+            this.options = { behavior: 'smooth', block: 'start' };
+        } else {
+            this.options = options;
+        }
+        this.scrollToSection = this.scrollToSection.bind(this);
+    }
 
-    function scrollToSection(event) {
+    scrollToSection(event) {
         event.preventDefault();
         const href = event.currentTarget.getAttribute('href');
         const section = document.querySelector(href);
 
-        section.scrollIntoView({
-            behavior: 'smooth',
-            block: 'start',
+        section.scrollIntoView(this.options);
+    }
+
+    addLinkEvent() {
+        this.internalLinks.forEach((link) => {
+            link.addEventListener('click', this.scrollToSection);
         });
     }
 
-    internalLinks.forEach((link) => {
-        link.addEventListener('click', scrollToSection);
-    });
+    init() {
+        if (this.internalLinks.length) this.addLinkEvent();
+        return this;
+    }
 }
