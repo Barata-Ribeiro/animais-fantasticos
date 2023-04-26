df8ce2a64426fe6ca0e9281f549efd6867c3a352
João M J Barata Ribeiro <j.mendes1920@gmail.com>
2023-04-26 06:42:38 -0300

    tabnav transformado em classe

--- js/modules/tab-nav.js
+++ js/modules/tab-nav.js
@@ -1,22 +1,41 @@
-export default function initTabNav() {
-    const tabMenu = document.querySelectorAll('[data-tab="menu"] li');
-    const tabContent = document.querySelectorAll('[data-tab="content"] section');
-
-    function activeTab(index) {
-        tabContent.forEach((section) => {
-            section.classList.remove('ativo');
-        });
-        const direcao = tabContent[index].dataset.anime;
-        tabContent[index].classList.add('ativo', direcao);
+export default class TabNav {
+    // Construtor recebe seletor de menu e seletor de conteúdo como parâmetros
+    constructor(menu, content) {
+        // Seleciona todos os itens do menu
+        this.tabMenu = document.querySelectorAll(menu);
+        // Seleciona todos os elementos de conteúdo
+        this.tabContent = document.querySelectorAll(content);
+        // Classe CSS para a aba ativa
+        this.activeClass = 'ativo';
     }
 
-    if (tabMenu.length && tabContent.length) {
-        tabContent[0].classList.add('ativo');
-
-        tabMenu.forEach((itemMenu, index) => {
-            itemMenu.addEventListener('click', () => {
-                activeTab(index);
-            });
+    // Ativa a aba correspondente ao índice fornecido
+    activeTab(index) {
+        // Remove a classe ativa de todas as seções de conteúdo
+        this.tabContent.forEach((section) => {
+            section.classList.remove(this.activeClass);
         });
+
+        // Adiciona a classe ativa e a direção à seção de conteúdo correspondente
+        const direcao = this.tabContent[index].dataset.anime;
+        this.tabContent[index].classList.add(this.activeClass, direcao);
+    }
+
+    // Adiciona eventos de clique às abas do menu
+    addTabNavEvent() {
+        this.tabMenu.forEach((itemMenu, index) => {
+            // Adiciona um ouvinte de evento de clique a cada item do menu
+            itemMenu.addEventListener('click', () => this.activeTab(index));
+        });
+    }
+
+    // Inicializa a funcionalidade de navegação por abas
+    init() {
+        // Verifica se os elementos do menu e conteúdo existem
+        if (this.tabMenu.length && this.tabContent.length) {
+            // Ativa a primeira aba e adiciona eventos de clique às abas do menu
+            this.activeTab(0);
+            this.addTabNavEvent();
+        }
     }
 }
--- js/script.js
+++ js/script.js
@@ -1,6 +1,6 @@
 import SmoothScroll from './modules/smooth-scroll.js';
 import Accordion from './modules/accordion.js';
-import initTabNav from './modules/tab-nav.js';
+import TabNav from './modules/tab-nav.js';
 import initModal from './modules/modal.js';
 import initTooltip from './modules/tooltip.js';
 import initDropdownMenu from './modules/dropdown-menu.js';
@@ -16,8 +16,10 @@ smoothScroll.init();
 const accordion = new Accordion('[data-anime="accordion"] dt');
 accordion.init();
 
+const tabNav = new TabNav('[data-tab="menu"] li', '[data-tab="content"] section');
+tabNav.init();
+
 initScrollAnimation();
-initTabNav();
 initModal();
 initTooltip();
 initDropdownMenu();
